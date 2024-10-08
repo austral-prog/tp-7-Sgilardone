c69d1205636e0f9409c00636e9dc49b0dfd191d0
Sgilardone <sgilardone@mail.austral.edu.ar>
2024-10-08 17:20:03 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,19 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(colors):
+    result = []
+    for index, color in enumerate(colors):
+        if color: 
+            result.append(f"{len(result)}. {color}")  
+    return result
+
+def enumerate_backwards(colors):
+    result = []
+    for index, color in enumerate(colors):
+        if color:  
+            result.append(f"{len(result)}. {color[::-1]}")  
+    return result
 
 
-def enumerate_backwards(list):
-    return list
+colors = ["Red", "Green", "", "White", "Black"]
+
+print(enumerate_list(colors))
+print(enumerate_backwards(colors))
